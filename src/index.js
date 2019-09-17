bacbfa2cec2f5947e7b3eff15355c2dbc9880d35
Josh Nho <joshik@Joshs-MacBook-Pro.local>
2019-09-17 16:44:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querySelector("form#create-task-form")
+  const taskList = document.querySelector("ul#tasks")
+  taskForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    let newTask = document.createElement("li")
+    // debugger
+    let newDesc = taskForm.newTaskDescription.value
+    const delButton = document.createElement("BUTTON")
+    delButton.innerText = "X"
+    // debugger
+    delButton.addEventListener("click", function(evt){
+      evt.preventDefault()
+      // debugger
+      evt.target.parentElement.remove()
+    })
+    // debugger
+    newTask.innerText = newDesc
+    newTask.appendChild(delButton)
+    // debugger
+    taskList.append(newTask)
+    
+  })
 });
